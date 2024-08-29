dcbd4de0e642ee860aa640e86f23d920a6a95d70
hansaguillon <hansaguillon@gmail.com>
2024-08-29 19:58:34 -0300

    cambios

--- e-backend/src/app.module.ts
+++ e-backend/src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-@Module({
-  imports: [CoursesModule, CourseTopicsModule, TopicsModule, CompanyModule, CourseMediaModule, BuyCoursesModule],
 import { BillModule } from './bill/bill.module';
 import { UsersModule } from './users/users.module';
 import { UserMediaModule } from './user_media/user_media.module';
 import { RolesModule } from './roles/roles.module';
 import { BillDetailModule } from './bill_detail/bill_detail.module';
 import { FavoritesModule } from './favorites/favorites.module';
-
 @Module({
   imports: [BillModule, UsersModule, UserMediaModule, RolesModule, BillDetailModule, FavoritesModule],
   controllers: [AppController],
