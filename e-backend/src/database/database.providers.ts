89de71f347ed08cfb28ff708eb9649e02893eb0c
hansaguillon <hansaguillon@gmail.com>
2024-10-14 20:16:47 -0300

    base de datos

--- e-backend/src/database/database.providers.ts
+++ e-backend/src/database/database.providers.ts
@@ -12,11 +12,11 @@ export const databaseProviders = [
             // Crea una nueva instancia de DataSource con la configuración especificada
             const dataSource = new DataSource({
                 type: 'mysql',                // Tipo de base de datos (MySQL)
-                host: 'localhost',            // Dirección del host de la base de datos
+                host: 'bnhspe0pbxzyjzpnrx7g-mysql.services.clever-cloud.com',            // Dirección del host de la base de datos
                 port: 3306,                   // Puerto en el que se ejecuta la base de datos
-                username: 'admin',             // Nombre de usuario para acceder a la base de datos
-                password: 'admin',             // Contraseña para acceder a la base de datos
-                database: 'einstein',     // Nombre de la base de datos a la que se conectará
+                username: 'u92ao8gwarijgwfw',             // Nombre de usuario para acceder a la base de datos
+                password: 'wtoUbubx8ggSBpMivo0u',             // Contraseña para acceder a la base de datos
+                database: 'bnhspe0pbxzyjzpnrx7g',     // Nombre de la base de datos a la que se conectará
                 entities: [
                     __dirname + '/../**/*.entity{.ts,.js}', // Ruta donde se encuentran las entidades
                 ],
