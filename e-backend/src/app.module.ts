eceb5c184a5560281cf6336faafc5c1e5626453f
hansaguillon <hansaguillon@gmail.com>
2024-10-18 11:51:26 -0300

    levantar la imagen

--- e-backend/src/app.module.ts
+++ e-backend/src/app.module.ts
@@ -17,9 +17,6 @@ import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-
 
 
 @Module({
@@ -30,10 +27,6 @@ import { join } from 'path';
         limit:40,
       }
     ]),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'), // La carpeta de tus archivos subidos
-      serveRoot: '/uploads', // La raíz desde donde se servirán los archivos
-    }),
     BillModule, UsersModule, RolesModule, BillDetailModule, FavoritesModule,CoursesModule, CourseTopicsModule, TopicsModule, CompanyModule, CourseMediaModule, BuyCoursesModule, RatingModule, CategoriesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, {provide: APP_GUARD, useClass: ThrottlerGuard}],
