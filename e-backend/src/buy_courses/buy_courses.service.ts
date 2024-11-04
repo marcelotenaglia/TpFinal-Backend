18e1a581a6ed84cad1da1868c8e4cb62a3619366
hansaguillon <hansaguillon@gmail.com>
2024-11-04 17:35:12 -0300

    compras

--- e-backend/src/buy_courses/buy_courses.service.ts
+++ e-backend/src/buy_courses/buy_courses.service.ts
@@ -26,12 +26,12 @@ export class BuyCoursesService {
 {
   const user = await this.userRepository.findOne({ where: { id: user_id } });
   if (!user) {
-    throw new Error('No se encontró un usuario con ese ID');
+    throw new NotFoundException('No se encontró un usuario con ese ID');
   }
 
   const course = await this.courseRepository.findOne({ where: { id: course_id } });
   if (!course) {
-    throw new Error('No se encontró un curso con ese ID');
+    throw new NotFoundException('No se encontró un curso con ese ID');
   }
 
   // Busca si el usuario ya compra el curso
